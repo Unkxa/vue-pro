5b7d94c0a6d8dad5478ff02ed5a69c07f68e843f
wangyu <wangyu@amberdata.cn>
2022-06-22 11:30:16 +0800

    first update

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { Button, Select } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' 
+import './main.css'
+
+Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about-vue">
+    <div class="about-">
+      <el-button @click="buttonHandelClick()" >默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import './AboutView.scss'
+// import * as black from './AboutView-black.scss'
+export default {
+  data:()=>{
+    return {
+      showThme : false,
+      themeUrl : './AboutView.scss',
+      white : './AboutView.scss',
+      black:'./AboutView-black.scss'
+    }
+  },
+  methods:{
+    buttonHandelClick() {
+      this.themeUrl = this.themeUrl == this.white ? this.black: this.white
+      console.log(this.themeUrl );
+    }
+  }
+}
+</script>
+  
+<style>
+
+.about-vue{
+  width: 100vw;
+  height: 100vh;
+  /* background-color: rgba(255, 255, 255, 0.547); */
+}
+</style>
